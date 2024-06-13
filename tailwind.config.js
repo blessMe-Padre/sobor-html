2bd1cf184b4dfe458ac092d05f8a65b32cdcaf01
Сергей Субботин <zarodiny@yandex.ru>
2024-06-13 16:52:00 +1000

    init

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+  ],
+  theme: {
+    screens: {
+      'sm': '768px',
+      'md': '1024px',
+      'lg': '1280px',
+      'xl': '1440px',
+    },
+
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      // ---------------------------------
+      'main-black': '#303030',
+      'default-black': '#000000',
+      'brown': '#D8D2C2',
+      'light-brown': '#EDEBE5',
+      'dark-brown': '#A99F8A',
+      'orange': '#DBBA61',
+    },
+
+    container: {
+      padding: '20px',
+      center: true
+    },
+    extend: {
+      fontFamily: {
+        'bodyFont': ['archangelsk', 'regular'],
+      }
+    }
+  },
+  plugins: [],
+}
+
