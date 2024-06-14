55b40edd3c81481013281f0ea010141616707945
Сергей Субботин <zarodiny@yandex.ru>
2024-06-14 13:39:51 +1000

    feat: добавляет стили

--- js/module/sliders.js
+++ js/module/sliders.js
@@ -8,15 +8,50 @@ export const initSlider = () => {
             spaceBetween: 30,
             slidesPerView: 1,
 
+            // navigation: {
+            //     nextEl: '.swiper-button-next',
+            //     prevEl: '.swiper-button-prev',
+            // },
+
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true
+            },
+        });
+    }
+
+    // слайдер "Отзывы"
+    const newsSection = document.querySelector('.news');
+    if (newsSection) {
+        const feedback = new Swiper('.news-swiper', {
+            loop: true,
+            spaceBetween: 30,
+            slidesPerView: 1,
+
             navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
+                nextEl: '.news-button-next',
+                prevEl: '.news-button-prev',
             },
 
             pagination: {
                 el: ".swiper-pagination",
                 clickable: true
             },
+
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                767: {
+                    slidesPerView: 2,
+                    spaceBetween: 15,
+                },
+
+                1023: {
+                    slidesPerView: 3,
+                    spaceBetween: 20,
+                },
+            }
         });
     }
 }
