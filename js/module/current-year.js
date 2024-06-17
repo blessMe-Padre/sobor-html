20b810d08ed600f9aff64a98577bacc8af1ea65c
Сергей Субботин <zarodiny@yandex.ru>
2024-06-17 14:44:23 +1000

    feat: добавляет футер

--- js/module/current-year.js
+++ js/module/current-year.js
@@ -1,7 +1,13 @@
 export const initCurrentYear = () => {
     const year = new Date().getFullYear();
-    const element = document.querySelector('.data-span');
+    const element = document.querySelectorAll('.data-span');
     if (element) {
-        element.innerText = year;
+        element.forEach(item => {
+            item.innerText = year;
+        });
     }
 }
+
+
+
+
